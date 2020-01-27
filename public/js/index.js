a58b14bbf35cefba56249e5948e343789c14bf0c
tanukiti <33892948+tanukiti@users.noreply.github.com>
2020-01-27 16:53:35 +0900

    add

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,11 @@
 $(function () {
-    $(".btn-gnavi").on("click", function () {
-        // ハンバーガーメニューの位置を設定
-        var rightVal = 0;
-        if ($(this).hasClass("open")) {
-            // 位置を移動させメニューを開いた状態にする
-            rightVal = -300;
-            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-            $(this).removeClass("open");
+    $(".burger-icon").on("click", function () {
+        if ($(this).hasClass("is-burger-nav-open")) {
+            $(this).removeClass("is-burger-nav-open");
+            $(".burger-nav").removeClass("open");
         } else {
-            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-            $(this).addClass("open");
+            $(this).addClass("is-burger-nav-open");
+            $(".burger-nav").addClass("open");
         }
-        $("#global-navi").stop().animate({
-            right: rightVal
-        }, 200);
     });
 });
